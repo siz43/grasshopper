c4371c33e89e7139351a2383fda4330ef29200dc
siz43 <91001691+siz43@users.noreply.github.com>
2021-09-20 21:39:53 +0900

    add new file

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// コンソールにHello World!を出力する
+console.log('Hello World!');
+
+function inYen(dollars,){
+  let amount = dollars * 113;
+  return amount;
+}
+console.log('20 US dollars is:');
+console.log(inYen(20) +' Japanese yen');
+console.log('100 US dollars is:');
+console.log(inYen(100) + ' Japanese yen');
